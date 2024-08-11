1dfe86e8c4f2c35e69e6a24fbb1293acc05d7211
Andre Oliveira <andregfo@gmail.com>
2024-08-11 16:53:47 +0000

    gerando numero aleatorio

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,13 @@
 alert("Boas vindas ao jogo do número secreto!")
 
-let numeroSecreto = 6;
+//let numeroSecreto = 6;
+
+let numeroSecreto = parseInt(Math.random() * 100+1);
 let acertou = false;
 let tentativas = 1;
 
 while (acertou != true){
-    let chute = prompt('Escolha um número entre 1 e 10');
+    let chute = prompt('Escolha um número entre 1 e 100');
 
     if( chute == numeroSecreto){
     //console.log(`Isso aí! Você descobriu o número secreto na ${tentativas}ª tentiva!`);
@@ -22,3 +24,7 @@ while (acertou != true){
         }
     }
 }
+
+//Operador ternário se          então                senão
+let palavra = tentativas > 10 ? "Precisa melhorar!" : "Você é um Gênio!"
+alert(palavra);
