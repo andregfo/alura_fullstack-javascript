3a0b9f65365f910e292d69005ec39ea07ff237d3
Andre Oliveira <andregfo@gmail.com>
2024-08-11 14:49:13 +0000

    Ajustes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -6,3 +6,10 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto){
     console.log('Isso aí! Você descobriu o número secreto!');
 }
+
+console.log('Rodando!')
+let cont = 1;
+while(cont < 11){
+    console.log(`Volta ${cont}`);
+    cont++;
+}
