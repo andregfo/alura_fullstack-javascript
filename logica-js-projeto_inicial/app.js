711a8b7bceb631be0ec9ea376e9a4eef55a8779a
Andre Oliveira <andregfo@gmail.com>
2024-08-11 15:22:02 +0000

    Jogo funcionando

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,24 @@
 alert("Boas vindas ao jogo do número secreto!")
 
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 6;
+let acertou = false;
+let tentativas = 1;
 
-if (chute == numeroSecreto){
-    console.log('Isso aí! Você descobriu o número secreto!');
+while (acertou != true){
+    let chute = prompt('Escolha um número entre 1 e 10');
+
+    if( chute == numeroSecreto){
+    //console.log(`Isso aí! Você descobriu o número secreto na ${tentativas}ª tentiva!`);
+    alert(`Isso aí! Você descobriu o número secreto na ${tentativas}ª tentiva!`);
+        acertou = true;
+    } else {
+        tentativas++;        
+        if(numeroSecreto > chute){
+            //console.log('O número secreto é MAIOR!');
+            alert('O número secreto é MAIOR!');
+        } else {
+            //console.log('O número secreto é MENOR!');
+            alert('O número secreto é MENOR!');
+        }
+    }
 }
